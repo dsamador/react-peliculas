7e504515a20e55e576bd7e2276c969c3de9f6184
David Amador <54488581+dsamador@users.noreply.github.com>
2021-11-08 22:06:34 -0500

    Listado de peliculas

---
+++ src/utils/Cargando.tsx
@@ -0,0 +1,4 @@
+export default function Cargando(){
+    return <img src="https://media4.giphy.com/media/sSgvbe1m3n93G/giphy.gif?cid=ecf05e473gkfydqinemgggon44lydhopq95bkjo5ltvauirg&rid=giphy.gif&ct=g"
+    alt="loading gif"/>
+}

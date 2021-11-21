2418d873a9cabe55b494112ab6afe3869d4b3074
David Amador <54488581+dsamador@users.noreply.github.com>
2021-11-20 20:10:14 -0500

    Seleccionando Fechas

--- src/actores/FormularioActores.tsx
+++ src/actores/FormularioActores.tsx
@@ -4,6 +4,7 @@ import FormGroupText from '../utils/FormGroupText';
 import Button from '../utils/Button';
 import { Link } from "react-router-dom";
 import * as Yup from 'yup';
+import FormGroupFecha from '../utils/FormGroupFecha';
 
 export default function FormularioActores(props:formularioActoresProps){
     return (
@@ -11,12 +12,14 @@ export default function FormularioActores(props:formularioActoresProps){
             initialValues={props.modelo}
             onSubmit={props.onSubmit}
             validationSchema={Yup.object({
-                nombre: Yup.string().required('Este campo es requerido').primeraLetraMayuscula()
+                nombre: Yup.string().required('Este campo es requerido').primeraLetraMayuscula(),
+                fechaNacimiento: Yup.date().nullable().required('Este campo es requerido')
             })}
         >
             {(formikProps) => (
                 <Form>
                     <FormGroupText campo="nombre" label="Nombre" />
+                    <FormGroupFecha label="Fecha Nacimiento" campo="fechaNacimiento" />
                     <Button disabled={formikProps.isSubmitting}
                         type="submit"
                     >Salvar</Button>

4b7b8f57515caaecb30af4b9e4fd7b8dde82a045
David Amador <54488581+dsamador@users.noreply.github.com>
2021-12-05 22:52:30 -0500

    Validaciones personalizadas, por atributo, por modelo

--- backend/PeliculasAPI/Entidades/Genero.cs
+++ backend/PeliculasAPI/Entidades/Genero.cs
@@ -1,14 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using PeliculasAPI.Validaciones;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PeliculasAPI.Entidades
 {
-    public class Genero
+    public class Genero: IValidatableObject//sirve para validar por modelo
     {
 
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength: 20, ErrorMessage = "{0} no debe tener mas de 20 caracteres")]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
 
         [Range(18, 120)]
@@ -19,5 +22,19 @@ namespace PeliculasAPI.Entidades
 
         [Url]
         public string URL { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+
+                if(primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayuscula",
+                    new string[] { nameof(Nombre) });
+                }
+            }
+        }
     }
 }
---
+++ backend/PeliculasAPI/Validaciones/PrimeraLetraMayusculaAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasAPI.Validaciones
+{
+    public class PrimeraLetraMayusculaAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;//Si el valor es null no importa
+                //en el modelo se puso el [Required] entonces no debemos rehacer la rueda
+            }
+            
+            var primeraLetra = value.ToString()[0].ToString();
+            if (primeraLetra != primeraLetra.ToUpper())
+            {
+                return new ValidationResult("La primera letra debe ser mayuscula");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}

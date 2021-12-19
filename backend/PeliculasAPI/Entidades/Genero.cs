e3432001a84d2b0666a390a0375aa19ec5243701
David Amador <54488581+dsamador@users.noreply.github.com>
2021-12-18 20:11:32 -0500

    limpieza completa

--- backend/PeliculasAPI/Entidades/Genero.cs
+++ backend/PeliculasAPI/Entidades/Genero.cs
@@ -4,40 +4,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PeliculasAPI.Entidades
 {
-    public class Genero: IValidatableObject//sirve para validar por modelo
+    public class Genero
     {
 
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido")]
-        [StringLength(maximumLength: 20, ErrorMessage = "{0} no debe tener mas de 20 caracteres")]
+        [StringLength(maximumLength: 50, ErrorMessage = "{0} no debe tener mas de 20 caracteres")]
         [PrimeraLetraMayuscula]
-        public string Nombre { get; set; }
-
-        [Range(18, 120)]
-        public int Edad { get; set; }
-
-        [CreditCard]
-        public string TarjetaDeCredito { get; set; }
-
-        [Url]
-        public string URL { get; set; }
-
-
-        //Estas validaciones por modelo solo se ejecutan cuando son exitosas las validaciones
-        //por atributo, sino no.
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (!string.IsNullOrEmpty(Nombre))
-            {
-                var primeraLetra = Nombre[0].ToString();
-
-                if(primeraLetra != primeraLetra.ToUpper())
-                {
-                    yield return new ValidationResult("La primera letra debe ser mayuscula",
-                    new string[] { nameof(Nombre) });
-                }
-            }
-        }
+        public string Nombre { get; set; }      
     }
 }
--- backend/PeliculasAPI/Startup.cs
+++ backend/PeliculasAPI/Startup.cs
@@ -9,9 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using PeliculasAPI.Controllers;
 using PeliculasAPI.Filtros;
-using PeliculasAPI.Repositorios;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -32,11 +30,7 @@ namespace PeliculasAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {            
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
-            services.AddResponseCaching();
-            services.AddTransient<IRepositorio, RepositorioEnMemoria>();
-            services.AddScoped<WeatherForecastController>();
-            services.AddTransient<MiFiltroDeAccion>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();            
             services.AddControllers(options =>
             {
                 options.Filters.Add(typeof(FiltroDeExcepcion));
@@ -49,38 +43,7 @@ namespace PeliculasAPI
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
-        {
-
-            app.Use(async (context, next) =>
-            //todo esto es para que se vean las respuestas JSON en la consola
-            {
-                using(var swapStream = new MemoryStream())
-                {
-                    var respuestaOriginal = context.Response.Body;
-                    context.Response.Body = swapStream;
-
-                    await next.Invoke();
-
-                    swapStream.Seek(0, SeekOrigin.Begin);
-                    string respuesta = new StreamReader(swapStream).ReadToEnd();
-                    swapStream.Seek(0, SeekOrigin.Begin);
-
-                    await swapStream.CopyToAsync(respuestaOriginal);
-                    context.Response.Body = respuestaOriginal;
-
-                    logger.LogInformation(respuesta);
-                }
-            });
-
-            app.Map("/mapa1", (app) =>
-            {
-                app.Run(async context =>
-                {
-                    await context.Response.WriteAsync("Estoy interceptando el pipeline");
-                });
-            });
-           
-
+        {                                
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -90,9 +53,7 @@ namespace PeliculasAPI
 
             app.UseHttpsRedirection();
 
-            app.UseRouting();
-
-            app.UseResponseCaching();
+            app.UseRouting();            
 
             app.UseAuthentication();
 

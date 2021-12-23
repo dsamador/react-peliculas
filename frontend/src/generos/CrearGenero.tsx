0b9ae489afc76cd3381a1ff9fc2b477a8bc508de
David Amador <54488581+dsamador@users.noreply.github.com>
2021-12-23 15:01:26 -0500

    mostrando error del web api

--- frontend/src/generos/CrearGenero.tsx
+++ frontend/src/generos/CrearGenero.tsx
@@ -3,24 +3,28 @@ import FormularioGeneros from './FormularioGeneros';
 import { generoCreacionDTO } from './generos.model';
 import { urlGeneros } from '../utils/endpoints';
 import { useHistory } from 'react-router-dom';
+import MostrarErrores from '../utils/MostrarErrores';
+import { useState } from 'react';
 
 export default function CrearGenero(){
     
     const history = useHistory();
+    const [errores, setErrores] = useState<string[]>([]);
 
     async function crear(genero: generoCreacionDTO){
         try{
             await axios.post(urlGeneros, genero);
             history.push('/generos');
         }
-        catch(error){
-
+        catch(error:any){
+            setErrores(error.response.data);
         }
     }
 
     return (        
         <>
             <h3>Crear genero</h3>
+            <MostrarErrores errores={errores} />
             <FormularioGeneros modelo={{nombre:''}}
                 onSubmit={async (valores)=>{
                     await crear(valores);
